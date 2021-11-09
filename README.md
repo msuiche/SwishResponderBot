3d9a4464ebc5cb2557d5670708bf4fd1dd36141c
Matt Suiche <msuiche@comae.com>
2021-11-09 11:40:50 -0500

    Add webhook support

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
-# Discord Responder Bot
-## Usage
-Add the bot to your server using [this link](https://discord.com/api/oauth2/authorize?client_id=900266608759537727&permissions=128&scope=bot).
+# SwishResponderBot
+![swishbot](images/swishbot.jpg)
 
-Make sure the user requested the logs via the command have a role called 'responders' assigned to them. Then just request the logs using this command:
-```
-!logs export
-```
+A Discord Incident Response Bot. Protecting Discord communities.
+Use this bot after being hacked to collect the required information for your investigation and understand what happened.
+## Usage
+Add the bot to your server using [this link](https://discord.com/api/oauth2/authorize?client_id=900266608759537727&permissions=536871040&scope=bot).
+
+Make sure the user requested the logs via the command have a role called 'responders' assigned to them. Then just request the logs using the following commands:
+
+### !swish.export logs
+Get a copy of the audit logs as a CSV file.
+### !swish.export webhooks
+Get a copy of the webhooks as a CSV file.
+
+## Permissions
+- View Audit Log
+- Manage Webhooks (_Personal request to anyone working for Discord for a `View Webhooks` permissions. Plzz_)
 
 ## Background Context
 Refer to the [CreatureToadz Twitter's thread](https://twitter.com/msuiche/status/1450702711981289472) I wrote on Twitter about how this project was born. 
--- main.py
+++ main.py
@@ -14,20 +14,22 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
 
-@bot.command(name='logs', help='Manage audit logs')
+@bot.command(name='swish.export', help='Collect logs required for the investigation.')
 @commands.has_role('responders')
 async def export_logs(ctx, command):
-    await ctx.author.send("Ola! Here is a copy of the audit logs, if you have any questions you can ping @msuiche#1337 on Discord or @msuiche on Twitter.")
+    await ctx.author.send("Yo! Here is a copy of the logs.")
+    await ctx.author.send("If you have any questions you can ping me @msuiche#1337 on Discord or @msuiche on Twitter.")
     # if command == "view":
     #    guild = ctx.message.guild
     #    async for entry in guild.audit_logs(limit=100):
     #        await ctx.author.send('{0.user} did {0.action} to {0.target}'.format(entry))
-    if command == "export":
-        fieldnames = ['created_at', 'category', 'id', 'user', 'action', 'target', 'before', 'after', 'reason', 'changes']
+    if command == "logs":
+        fieldnames = ['created_at', 'category', 'id', 'user', 'action', 'target', 
+                    'before', 'after', 'reason', 'changes']
         now = datetime.now()
         date_time = now.strftime("%Y%m%d-%H%M%S")
         guild = ctx.message.guild
-        filename = guild.name + '_' + date_time + '.csv'
+        filename = guild.name + '_' + date_time + '_auditlogs.csv'
         with open(filename, 'w', encoding='UTF8', newline='') as f:
             writer = csv.DictWriter(f, fieldnames=fieldnames)
             writer.writeheader()
@@ -50,9 +52,46 @@ async def export_logs(ctx, command):
         timestamp = now.strftime("%d/%m/%Y %H:%M:%S")
         print('[%s] %s on %s requested audit logs.' % (timestamp, ctx.author.name, guild.name))
 
+    elif command == "webhooks":
+        fieldnames = ['created_at', 'avatar', 'avatar_url', 'channel', 'channel_id',
+                    'guild', 'guild_id', 'id', 'name', 'token',
+                    'type', 'url', 'user']
+        now = datetime.now()
+        date_time = now.strftime("%Y%m%d-%H%M%S")
+        guild = ctx.message.guild
+        filename = guild.name + '_' + date_time + '_webhooks.csv'
+
+        webhooks = await guild.webhooks()
+        await ctx.author.send("There are a total of %d webhooks in this guild." % (len(webhooks)))
+
+        with open(filename, 'w', encoding='UTF8', newline='') as f:
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+            for entry in webhooks:
+                writer.writerow({
+                    'created_at': entry.created_at,
+                    'avatar': entry.avatar,
+                    'avatar_url': entry.avatar_url,
+                    'channel': entry.channel,
+                    'channel_id': entry.channel_id,
+                    'guild': entry.guild,
+                    'guild_id': entry.guild_id,
+                    'id': entry.id,
+                    'name': entry.name,
+                    'token': entry.token,
+                    'type': entry.type,
+                    'url': entry.url,
+                    'user': entry.user,
+                    })
+            f.close()
+            await ctx.author.send(file=discord.File(filename))
+
+        timestamp = now.strftime("%d/%m/%Y %H:%M:%S")
+        print('[%s] %s on %s requested webhooks lists.' % (timestamp, ctx.author.name, guild.name))
+
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
-        await ctx.send('You do not have the correct role for this command.')
+        await ctx.send('You do not have the "responders" role for this command.')
 
 bot.run(TOKEN)
